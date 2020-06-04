7b085fb0ba1c48551b4615a4a4707ddc3884351d
chaturanga <chaturanga@huubap.com>
2020-06-04 14:46:37 +0800

    Added slack bot conversation

--- index.js
+++ index.js
@@ -2,49 +2,42 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new SlackBot({
-  token: 'xoxb-1160973993026-1154923438406-S2tpUbHWCrP1H7okym3CC8I4',
-  name: 'jokebot'
+  token: 'xoxb-1160973993026-1154923438406-jWrA84TyqEq4QonpRyItl2b4',
+  name: 'task_update_bot'
 });
+
+const OHAYO = 'Ohayo';
+const YES = 'YES';
+const POST = 'POST';
+
+let channels = [];
+let users = [];
+let tasks = [];
 
 // Start Handler
 bot.on('start', () => {
-  const params = {
-    icon_emoji: ':smiley:'
-  };
 
   bot.getChannels().then(result => {
     console.log('Channels...!!!');
-    console.log(result);
+    channels = result;
   });
 
   bot.getUsers().then(result => {
     console.log('Users...!!!');
-    console.log(result);
+    users = result.members;
   });
 
-
-  bot.postMessageToChannel(
-      'general',
-      ' <form>\n' +
-      '  <label for="fname">First name:</label><br>\n' +
-      '  <input type="text" id="fname" name="fname"><br>\n' +
-      '  <label for="lname">Last name:</label><br>\n' +
-      '  <input type="text" id="lname" name="lname">\n' +
-      '</form> ',
-      params
-  );
+  postToChannel('general', 'task_update_bot started...!!!');
 });
 
 // Error Handler
 bot.on('error', err => console.log(err));
-
 
 // Open connection
 bot.on('open', () => console.log('Socket Opened...!!!'));
 
 // Close connection
 bot.on('close', () => console.log('Socket Closed...!!!'));
-
 
 // Message Handler
 bot.on('message', data => {
@@ -53,60 +46,72 @@ bot.on('message', data => {
     return;
   }
 
-  handleMessage(data.text);
+  handleMessage(data);
 });
 
 // Respons to Data
-function handleMessage(message) {
-
-  if(!message)
+function handleMessage(data) {
+  if (!data)
     return;
-  // console.log(message);
-  if (message.includes('chucknorris')) {
-    chuckJoke();
-  } else if (message.includes(' yomama')) {
-    yoMamaJoke();
-  } else if (message.includes(' random')) {
-    randomJoke();
-  } else if (message.includes(' help')) {
-    runHelp();
+   if (data.text.includes(OHAYO)) {
+    let user = getUser(data);
+    let task = getUserTask(data);
+    if(task){
+      task.yesterday = -1;
+      task.today = -1;
+      task.blocker = -1;
+    }
+    postToUser(user.name, `Hello ${user.real_name} it\'s time to send the task update, ready? \n Type \'YES\' or \'NO\'`);
+  } else if (data.text.includes(YES)) {
+    let user = getUser(data);
+    let task = {user_id: user.id, yesterday: -1, today: -1, blocker: -1};
+    tasks.push(task);
+    postToUser(user.name, `What did you do previous working day?`);
+  } else if (data.text.includes(POST)) {
+    console.log('POST');
+    let user = getUser(data);
+    let task = getUserTask(data);
+    console.log(task);
+    // tasks.splice(task);
+    postToChannel('huubap_task_update', task);
+  } else if (getUserTask(data)) {
+    let user = getUser(data);
+    let task = getUserTask(data);
+    if (task.yesterday === -1) {
+      task.yesterday = data.text;
+      postToUser(user.name, `What do you do today?`);
+    } else if (task.today === -1) {
+      task.today = data.text;
+      postToUser(user.name, `Any blockers?`);
+    } else if (task.blocker === -1) {
+      task.blocker = data.text;
+      postToUser(user.name, `Thanks for your status, here is the summary`);
+      postToUser(user.name, `Summary`);
+      postToUser(user.name, `Post to channel \n Type \'POST\'`);
+    }
   }
 }
 
-// Tell a Chuck Norris Joke
-function chuckJoke() {
-  axios.get('http://api.icndb.com/jokes/random').then(res => {
-    const joke = res.data.value.joke;
-
-    const params = {
-      icon_emoji: ':laughing:'
-    };
-
-    bot.postMessageToChannel('general', `Chuck Norris: ${joke}`, params);
-  });
+function getUser(data) {
+  return users.find(item => item.id === data.user);
 }
 
-// Tell a Yo Mama Joke
-function yoMamaJoke() {
-  axios.get('http://api.yomomma.info').then(res => {
-    const joke = res.data.joke;
-
-    const params = {
-      icon_emoji: ':laughing:'
-    };
-
-    bot.postMessageToChannel('general', `Yo Mama: ${joke}`, params);
-  });
+function getUserTask(data) {
+  return tasks.find(item => item.user_id === data.user);
 }
 
-// Tell a Random Joke
-function randomJoke() {
-  const rand = Math.floor(Math.random() * 2) + 1;
-  if (rand === 1) {
-    chuckJoke();
-  } else if (rand === 2) {
-    yoMamaJoke();
-  }
+function postToUser(userChannel, message) {
+  const params = {
+    icon_emoji: ':laughing:'
+  };
+  bot.postMessageToUser(userChannel, message, params);
+}
+
+function postToChannel(channel, message) {
+  const params = {
+    icon_emoji: ':laughing:'
+  };
+  bot.postMessageToChannel(channel, message, params);
 }
 
 // Show Help Text
